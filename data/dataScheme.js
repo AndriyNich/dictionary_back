e132d586ddc09b45abc4f85e0553fdbd97757fab
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-11-20 17:01:19 +0200

    add schemas data

---
+++ data/dataScheme.js
@@ -0,0 +1,27 @@
+const words = {
+  userId: "Object",
+  groupsId: [0],
+  en: [""],
+  ua: [""],
+  statictics: {
+    "en-ua": {
+      visible: 0,
+      error: 0,
+    },
+    "ua-en": {
+      visible: 0,
+      error: 0,
+    },
+  },
+};
+
+const langs = {
+  shortName: "",
+  name: "",
+  fullName: "",
+};
+
+const groups = {
+  name: "",
+  fullName: "",
+};
